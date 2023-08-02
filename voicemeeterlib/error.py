e96151cd5a678a8e3fa8ec01517cbd1068f7c2fb
onyx-and-iris <code@onyxandiris.online>
2023-08-02 15:42:45 +0100

    InstallError and CAPIError classes now subclass VMError
    minor version bump

--- voicemeeterlib/error.py
+++ voicemeeterlib/error.py
@@ -1,19 +1,22 @@
-class InstallError(Exception):
-    """Exception raised when installation errors occur"""
+class VMError(Exception):
+    """Base VM Exception class. Raised when general errors occur."""
 
-
-class CAPIError(Exception):
-    """Exception raised when the C-API returns error values"""
-
-    def __init__(self, fn_name, code, msg=None):
-        self.fn_name = fn_name
-        self.code = code
-        self.message = msg if msg else f"{fn_name} returned {code}"
+    def __init__(self, msg):
+        self.message = msg
         super().__init__(self.message)
 
     def __str__(self):
         return f"{type(self).__name__}: {self.message}"
 
 
-class VMError(Exception):
-    """Exception raised when general errors occur"""
+class InstallError(VMError):
+    """Exception raised when installation errors occur"""
+
+
+class CAPIError(VMError):
+    """Exception raised when the C-API returns an error code"""
+
+    def __init__(self, fn_name, code, msg=None):
+        self.fn_name = fn_name
+        self.code = code
+        super(CAPIError, self).__init__(msg if msg else f"{fn_name} returned {code}")
